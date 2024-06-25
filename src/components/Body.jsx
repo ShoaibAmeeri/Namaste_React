d3ec3ddadb553dac94b7b0637fe746fca0ab73cc
ShoaibAmeeri <shoaibameeri56@gmail.com>
2024-06-25 16:16:55 +0500

    episode 6 part 6 completed

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,12 +2,10 @@ import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 
-
 const Body = () => {
-  
   let [ResList, setResList] = useState([]);
-  useEffect( () => {
-     fetchData();
+  useEffect(() => {
+    fetchData();
   }, []);
 
   const fetchData = async () => {
@@ -15,20 +13,40 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
 
-    const json = await data.json()
-    setResList(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-
+    const json = await data.json();
+    setResList(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
 
- 
+  const [searchTxt, setSearchTxt] = useState("");
 
-  return ResList.length === 0 ? <Shimmer/> :   (
+  return ResList.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search_box"
+            value={searchTxt}
+            onChange={(e) => setSearchTxt(e.target.value)}
+          />
+          <button className="search_btn" onClick={() =>{
+             console.log(searchTxt)
+             
+            const filteredRes =  ResList.filter((res) => res.info.name.toLowerCase().includes(searchTxt.toLowerCase()))
+            setResList(filteredRes)
+             }}>
+            search
+          </button>
+        </div>
+
         <button
           className="filter_btn"
           onClick={() => {
-            ResList = ResList.filter((res) => res.info.avgRating > 4);
+            ResList = ResList.filter((res) => res.info.avgRating > 4.3);
             setResList(ResList);
           }}
         >
@@ -36,14 +54,11 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {ResList.map((restaurant, i) => (
-          <>
-          <RestaurantCard key={i} resData={restaurant.info} />
-       {   console.log(restaurant.info.cloudinaryImageId)}
-          </>
-         ))} 
+        {ResList.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant.info} />
+        ))}
 
-         <div className="carddd"></div>
+        <div className="carddd"></div>
       </div>
     </div>
   );
--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,22 +1,18 @@
-import React from 'react'
-
+import React from "react";
 
 function Shimmer() {
   return (
-   <div className="shimer_container">
-    <div className="shimer_card"></div>
-    <div className="shimer_card"></div>
-    <div className="shimer_card"></div>
-    <div className="shimer_card"></div>
-    <div className="shimer_card"></div>
-    <div className="shimer_card"></div>
-    <div className="shimer_card"></div>
-    <div className="shimer_card"></div>
-   </div>
-  )
+    <div className="shimer_container">
+      <div className="shimer_card"></div>
+      <div className="shimer_card"></div>
+      <div className="shimer_card"></div>
+      <div className="shimer_card"></div>
+      <div className="shimer_card"></div>
+      <div className="shimer_card"></div>
+      <div className="shimer_card"></div>
+      <div className="shimer_card"></div>
+    </div>
+  );
 }
 
-export default Shimmer
-
-
-
+export default Shimmer;
