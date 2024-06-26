bbdd0e32220f711522a5083dd130609fc2c53ffd
ShoaibAmeeri <shoaibameeri56@gmail.com>
2024-06-26 14:03:52 +0500

    episode 7 part 3 completed

--- src/app.js
+++ src/app.js
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
-
-
-
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 
 
@@ -14,12 +13,29 @@ const AppLayout = () => {
   return (
     <div className="app">
       <h1>hello</h1>
-    <Header/>
-      <Body/>
+      <Header />
+      <Body />
     </div>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element: <AppLayout/>,
+    errorElement: <Error/>
+
+  },
+  {
+    path:"/about",
+    element: <About/>
+  },
+  {
+    path:"/contact",
+    element: <Contact/>
+  },
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
