794ef05a3839142fef569598d33cd8adf30c7160
ShoaibAmeeri <shoaibameeri56@gmail.com>
2024-07-01 07:17:02 +0500

    compeleted episode 8 part 3

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -13,11 +13,19 @@ class UserClass extends React.Component {
 
   render() {
     const { name, location, contact } = this.props;
-    const{count, count2} = this.state
+    let{count, count2} = this.state
     return (
       <div className="user_card">
         <h1> count {count}</h1>
         <h1> count {count2}</h1>
+        <button onClick={()=>{
+          this.setState({
+            count : ++this.state.count,
+            count2 : ++this.state.count2
+          })
+          console.log(count)
+        }}>count Increment</button>
+      
         <h2>name : {name}</h2>
         <h3>location : {location}</h3>
         <h4>Contact : {contact}</h4>
