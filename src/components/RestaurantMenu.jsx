ee961375c5950bef72849e034a157745064ecde9
ShoaibAmeeri <shoaibameeri56@gmail.com>
2024-07-07 12:36:17 +0500

    compeleted episode 9 part 1

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,30 +1,16 @@
-import React, { useEffect, useState } from "react";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 import Shimmer from "./Shimmer";
 import ItemMenuCard from "./itemMenuCard";
 import { useParams } from "react-router-dom";
-import { MENU_URL } from "../utils/constans";
 
 function RestaurantMenu() {
-  const [resInfo, setResInfo] = useState(null);
-  useEffect(() => {
-    fetchMenu();
-  }, []);
   const { resId } = useParams();
-  console.log(resId);
-
-  const fetchMenu = async () => {
-    const data = await fetch(MENU_URL + resId);
-
-    const json = await data.json();
-
-    setResInfo(json.data);
-  };
+  const resInfo = useRestaurantMenu(resId);
 
   if (resInfo === null) return <Shimmer />;
 
   const { itemCards } =
     resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards[2].card.card;
-  console.log(itemCards);
 
   const { name, cuisines, costForTwoMessage } = resInfo.cards[2].card.card.info;
 
@@ -37,11 +23,9 @@ function RestaurantMenu() {
       <ul>
         {itemCards.map((item) => (
           <li key={item.card.info.id}>
-            {" "}
-            <ItemMenuCard data={item.card.info} />{" "}
+            <ItemMenuCard data={item.card.info} />
           </li>
         ))}
-        {/* {itemCards.map((item)=> <li>{item.card.info.name} -- {item.card.info.price}</li> )} */}
       </ul>
     </div>
   );
